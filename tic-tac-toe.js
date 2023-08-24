91f38f92654baf133f328c85eae601e7c87710e4
Conrad Hope <conjhope@outlook.com>
2023-08-24 15:48:35 -0600

    8/24 commit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,10 @@
 let playerX = $('xBtn');
 let playerO = $('oBtn');
 
-let playerTurn = $('btn_turn')
-let playerStart= $('btn_start')
+let playerTurn = $('btn_turn');
+let playerStart= $('btn_start');
+
+let gameTable = $('tttTable');
 
 // $('xWin').epmty()
 
@@ -10,28 +12,37 @@ const startGameBtn = () => {
 
 }
 
+const boxSelectionX = (idOfSelectionX) => {
+    
+}
+
+const boxSelectionO = (idOfSelectionO) => {
+
+}
+
 const endTurnBtn = () => {
     
 }
 
-let xWins = $('xWin')
-let oWins = $('oWin')
-let draw = $('drawGame')
+let xWins = $('xWin');
+let oWins = $('oWin');
+let draw = $('drawGame');
 
 const endGameBtn = (idOfAlert) => {
     if (playerX === ['1,2,3'] || ['4,5,6'] || ['7,8,9'] || ['1,4,7'] || ['2,5,8'] || ['3,6,9'] || ['1,5,9'] || ['3,5,7']) {
         $(idOfAlert).text('gameOver')
         xWins.text('xWin')
     } else if (playerO === ['1,2,3'] || ['4,5,6'] || ['7,8,9'] || ['1,4,7'] || ['2,5,8'] || ['3,6,9'] || ['1,5,9'] || ['3,5,7']) {
-        $(idOfAlert).text('gameOver')
-        oWins.text('oWin')
+        $(idOfAlert).text('gameOver');
+        oWins.text('oWin');
     } else {
-        $(idOfAlert).text('gameOver')
-        draw.text('drawGame')
+        $(idOfAlert).text('gameOver');
+        draw.text('drawGame');
     }
 }
 
-const clearBoardBtn = () => {
-
+const clearBoardBtn = (clearBoard) => {
+    $(clearBoard).remove();
+    $('tttTable').value='';
 }
 
