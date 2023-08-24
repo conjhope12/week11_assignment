77d8d62aa33d484032f972280b24fa21a7cb80f0
Conrad Hope <conjhope@outlook.com>
2023-08-23 21:15:22 -0600

    8/23 commit

---
+++ tic-tac-toe.js
@@ -0,0 +1,37 @@
+let playerX = $('xBtn');
+let playerO = $('oBtn');
+
+let playerTurn = $('btn_turn')
+let playerStart= $('btn_start')
+
+// $('xWin').epmty()
+
+const startGameBtn = () => {
+
+}
+
+const endTurnBtn = () => {
+    
+}
+
+let xWins = $('xWin')
+let oWins = $('oWin')
+let draw = $('drawGame')
+
+const endGameBtn = (idOfAlert) => {
+    if (playerX === ['1,2,3'] || ['4,5,6'] || ['7,8,9'] || ['1,4,7'] || ['2,5,8'] || ['3,6,9'] || ['1,5,9'] || ['3,5,7']) {
+        $(idOfAlert).text('gameOver')
+        xWins.text('xWin')
+    } else if (playerO === ['1,2,3'] || ['4,5,6'] || ['7,8,9'] || ['1,4,7'] || ['2,5,8'] || ['3,6,9'] || ['1,5,9'] || ['3,5,7']) {
+        $(idOfAlert).text('gameOver')
+        oWins.text('oWin')
+    } else {
+        $(idOfAlert).text('gameOver')
+        draw.text('drawGame')
+    }
+}
+
+const clearBoardBtn = () => {
+
+}
+
